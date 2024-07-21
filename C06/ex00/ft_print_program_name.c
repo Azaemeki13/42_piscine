46f01616c411f263c4359ee15a5d1dfa850ad611
Charles Auffret <cauffret@c1r7s1.42luxembourg.lu>
2024-07-22 01:51:24 +0200

    Yezai

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauffret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 00:48:26 by cauffret          #+#    #+#             */
+/*   Updated: 2024/07/22 00:59:58 by cauffret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 0)
+		ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauffret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 01:00:51 by cauffret          #+#    #+#             */
+/*   Updated: 2024/07/22 01:00:52 by cauffret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+    int i;
+
+    i = 1;
+            while (i != argc)
+            {
+                ft_putstr(argv[i]);
+                ft_putchar('\n');
+                i++;
+            }
+	return (0);
+}
