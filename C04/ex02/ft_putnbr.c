c6beb9261a3c26f33f3996b6ac67789fe24eebde
Charles Auffret <cauffret@c2r6s3.42luxembourg.lu>
2024-07-22 20:55:42 +0200

    yez

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -21,16 +21,15 @@ void	ft_putnbr(int nb)
 {
 	if (nb == -2147483648)
 	{
-		ft_putchar('-');
-		ft_putchar('2');
-		nb = 147483648;
+		write(1, "-2147483648", 11);
+		return ;
 	}
 	if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if (nb > 10)
+	if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
 	}
